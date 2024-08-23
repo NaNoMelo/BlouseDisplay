6a1febe50a42dff3ae1862ee372043a392b53562
NaNoPi <nathan.lamey68@orange.fr>
2024-08-23 15:59:38 +0200

    Created a Debouncer Class

---
+++ lib/Debouncer/Debouncer.cpp
@@ -0,0 +1,33 @@
+#include "Debouncer.hpp"
+
+Debouncer::Debouncer(short pin, unsigned int debounceDelay) {
+  _pin = pin;
+  pinMode(pin, INPUT_PULLUP);
+
+  _debounceDelay = debounceDelay;
+  _lastDebounceTime = millis();
+
+  _state = digitalRead(pin);
+  _lastState = _state;
+}
+
+bool Debouncer::read() {
+  _lastState = _state;
+  bool reading = digitalRead(_pin);
+
+  if (reading != _lastRead) {
+    _lastDebounceTime = millis();
+  }
+
+  if ((millis() - _lastDebounceTime) > _debounceDelay) {
+    if (reading != _state) {
+      _state = reading;
+    }
+  }
+  _lastRead = reading;
+  return _state;
+}
+
+bool Debouncer::isFallingEdge() { return !_state && _lastState; }
+
+bool Debouncer::isRisingEdge() { return _state && !_lastState; }
